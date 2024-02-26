a31c8c22628d440d2e2fa23a215593992c4159be
Csorasky <112544905+Csorasky@users.noreply.github.com>
2024-02-26 18:33:22 +0800

    MSTANet_loss.py

--- MSTANet_loss.py
+++ MSTANet_loss.py
@@ -54,7 +54,7 @@ class SimVP(Base_method):
     def diff_div_reg(self, pred_y, batch_y, tau=0.1, eps=1e-12):
         B, T, C = pred_y.shape[:3]
         if T <= 2:  return 0
-        # 更改下边两行
+   
         gap_pred_y = (pred_y[:, 1:] - pred_y[:, :-1]).reshape(B, T - 1, -1)
         gap_batch_y = (batch_y[:, 1:] - batch_y[:, :-1]).reshape(B, T - 1, -1)
         softmax_gap_p = F.softmax(gap_pred_y / tau, -1)
